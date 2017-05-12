cd113b8c72466427fde5f1bfd86b2c56b44c18db
reazur <reazur@ares2.bio.brandeis.edu>
2017-05-12 19:23:39 -0400

    Add code to sort sam file, this fixes an error in the code

--- CODE/trim_and_align_gDNA.sh
+++ CODE/trim_and_align_gDNA.sh
@@ -4,7 +4,7 @@
 #----- Update the following varibles
 bowtie_indexes="/data/theia/analysis/dm3/Sequence/bowtie_indexes/genome"
 TRIMMOMATIC_DIR="/home/reazur/SOFTWARE"
-TRIBE_DIR="/data/theia/reazur/editing/github/TRIBE/CODE"
+#TRIBE_DIR="/data/theia/reazur/editing/github/TRIBE/CODE"
 
 #---------------------
 
@@ -14,7 +14,8 @@ trim_input=$file
 trim_outfile=$prefix.trim.fastq 
 avgquality="30"
 
-#trim the library as needed, here we are trimming the first 6 and last six base of reads that that 50 base long. The parameter for Trimmomatics needs to be adjusted based on the quality of the reads and length of the library
+#--------IMPORTATNT------#
+#trim the library as needed, here we are trimming the first 6 and last six base of reads that that 50 base long. The parameter for Trimmomatics needs to be adjusted based on the quality of the reads and length of the read
 java -jar $TRIMMOMATIC_DIR/Trimmomatic-0.30/trimmomatic-0.30.jar SE -phred33 $trim_input $trim_outfile CROP:45 HEADCROP:6 LEADING:25 TRAILING:25 AVGQUAL:$avgquality MINLEN:19
 
 
@@ -25,3 +26,13 @@ samtools view -Sh -q 10 $bowtie2_out >$prefix".sam"
 rm $bowtie2_out
 
 
+#following code tested with samtools 1.3.1, you might have to tweak it a bit bases your installed verison of samtools (these flags can be problematic for older version of samtools: -@, -o)
+samtools view -@ 6 -bSh  $prefix".sam" >  $prefix".bam"
+
+sort_out=$prefix".sort.bam"
+samtools sort -@ 6 $prefix".bam" -o $sort_out
+
+samtools view -@ 4 -h $sort_out > $prefix".sort.sam"
+
+rm $prefix".sam"
+rm $prefix".bam"
